60829a3a1d21b82d8323408400c47ca4424c70fc
sepehr emami <sepehr7890@gmail.com>
2024-02-01 17:45:48 +0330

    feat: BaseModel is now a thing here

--- config/settings.py
+++ config/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "core",
-    # "shop",
+    "shop",
 ]
 
 MIDDLEWARE = [
--- core/models.py
+++ core/models.py
@@ -1,3 +1,20 @@
+
 from django.db import models
 
-# Create your models here.
+
+class BaseModel(models.Model):
+    """
+    Common attributes of all models
+    All models extend BaseModel
+    """
+
+    is_deleted = models.BooleanField(default=False)
+    creation_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+    def delete(self, using=None, keep_parents=False):
+        self.is_deleted = True
+        self.save()
